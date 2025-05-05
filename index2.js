fdb8e6397e05dcbb42e46c172fce85350d95f95e
FA2004T <fatma.almandhari05@gmail.com>
2025-05-05 17:42:32 +0400

    Add files via upload

--- index2.js
+++ index2.js
@@ -1,7 +1,15 @@
 let countel=document.getElementById("count-el")
-
+let saveel=document.getElementById("save-el")
 let count =0;
+
 function increment(){
     count += 1;
     countel.innerText=count;
+}
+
+function save(){
+    let countstr= count+ " - ";
+    saveel.innerHTML+=countstr;
+    countel.innerText=0;
+    count =0 ;
 }
